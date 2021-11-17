169d2c053fefe43698e528473c1aec547c582fd1
stephen.easley-walsh <stephen.easley-walsh@studygroup.com>
2021-11-17 11:56:52 +0000

    Update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Write some code that asks for a 4 digit number (can't start with 0) that does not end in 0 and is an even number
+# Write a program that asks for a 4 digit number that is even and also does not end with a 0
+
+ 1235 IS BAD BECAUSE IT IS NOT EVEN
+ 1236 IS GOOD BECAUSE IT IS EVEN
+ 1220 IS BAD BECAUSE IT ENDS WITH 0
+ 123 IS BAD BECAUSE IT IS NOT 4 digit
+ 12345 IS BAD BECAUSE IT IS NOT 4 digit
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-x = int(input("Give me a four digit number that does not end with 0 and is even."))
+x = int(input("Give me a four digit number that is even but does not end with 0"))
 
 ####### The nice way to do it ####### EASY TO READ!!!
-if x >= 1000 and x <= 9999 and x%2 == 0:
+if x >= 1000 and x <= 9999 and x%2 == 0 and not x%10 == 0:
     print("Good job!")
 else:
     print("Nope, that's not right!")
@@ -10,13 +10,16 @@ else:
 if x >= 1000:
     if x <= 9999:
         if x%2 == 0:
-            print("Good job!")
+            if not x%10 == 0:
+                print("Good job!")
+            else:
+                print("Nope, that's not right! It ends with 0")
         else:
-            print("Nope, that's not right!")
+            print("Nope, that's not right! It is not even")
     else:
-        print("Nope, that's not right!")
+        print("Nope, that's not right! It is too big")
 else:
-    print("Nope, that's not right!")
+    print("Nope, that's not right! IT is too small")
 
 #quit
 quit()
